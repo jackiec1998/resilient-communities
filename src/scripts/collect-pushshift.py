46b422ca822fc3ae273f44f3bcf3f685d35628d6
Jackie Chan <jackiec1998@gmail.com>
2022-06-27 23:04:26 -0500

    Edited the Pushhshift script to output to a different collection and collecting at a more recent date.

--- src/scripts/collect-pushshift.py
+++ src/scripts/collect-pushshift.py
@@ -11,12 +11,15 @@
         ps aux | grep 'collect-popular.py'
 '''
 
-from psaw import PushshiftAPI
+from psaw import PushshiftAPI as psaw
 from pymongo import MongoClient
 import datetime as dt
 import os
 import time
 import sys
+import warnings
+
+warnings.filterwarnings('ignore')
 
 os.chdir('/shared/jackie/resilient-communities')
 
@@ -42,25 +45,26 @@ def log_report(message):
 
 try:
     client = MongoClient('localhost', 27017)
-    comments = client.pushshift_comments
-    threads = client.pushshift_threads
+    comments = client.resilient.pushshift_comments
 except Exception as e:
     log_message('Database did not connect successfully.')
     sys.exit()
 
-api = PushshiftAPI()
-
+psaw = psaw()
 
 def collect_comments(subreddit):
     while True:
         try:
-            if subreddit in comments.list_collection_names():
-                start = int(next(comments[subreddit].find({})
-                                 .sort('created_utc', -1).limit(1))['created_utc'])
-            else:
-                start = int(dt.datetime(2020, 1, 1).timestamp())
+            most_recent = comments.find_one({'subreddit': subreddit}, 
+                {'created_utc': 1}, sort=[('created_utc', -1)])
 
-            query = api.search_comments(
+            if most_recent == None:
+                start = int(dt.datetime(2021, 6, 1).timestamp())
+            else:
+                start = most_recent
+
+
+            query = psaw.search_comments(
                 subreddit=subreddit,
                 sort_type='created_utc',
                 sort='asc',
@@ -70,7 +74,7 @@ def collect_comments(subreddit):
             start = time.time()
 
             for i, comment in enumerate(query):
-                comments[subreddit].insert_one(comment.d_)
+                comments.insert_one(comment.d_)
 
                 if (i + 1) % 10_000 == 0:
                     timestamp = dt.datetime.fromtimestamp(
@@ -91,42 +95,42 @@ def collect_comments(subreddit):
     log_report(f'r/{subreddit} comments completed.')
 
 
-def collect_threads(subreddit):
-    while True:
-        try:
-            if subreddit in threads.list_collection_names():
-                start = int(next(threads[subreddit].find({})
-                                 .sort('created_utc', -1).limit(1))['created_utc'])
-            else:
-                start = int(dt.datetime(2020, 1, 1).timestamp())
+# def collect_threads(subreddit):
+#     while True:
+#         try:
+#             if subreddit in threads.list_collection_names():
+#                 start = int(next(threads[subreddit].find({})
+#                                  .sort('created_utc', -1).limit(1))['created_utc'])
+#             else:
+#                 start = int(dt.datetime(2020, 1, 1).timestamp())
 
-            query = api.search_submissions(
-                subreddit=subreddit,
-                sort_type='created_utc',
-                sort='asc',
-                after=start
-            )
+#             query = api.search_submissions(
+#                 subreddit=subreddit,
+#                 sort_type='created_utc',
+#                 sort='asc',
+#                 after=start
+#             )
 
-            start = time.time()
+#             start = time.time()
 
-            for i, thread, in enumerate(query):
-                threads[subreddit].insert_one(thread.d_)
+#             for i, thread, in enumerate(query):
+#                 threads[subreddit].insert_one(thread.d_)
 
-                if (i + 1) % 10_000 == 0:
-                    timestamp = dt.datetime.fromtimestamp(
-                        thread.created_utc).strftime('%x %I:%M:%S %p')
+#                 if (i + 1) % 10_000 == 0:
+#                     timestamp = dt.datetime.fromtimestamp(
+#                         thread.created_utc).strftime('%x %I:%M:%S %p')
 
-                    seconds_elapsed = time.time() - start
+#                     seconds_elapsed = time.time() - start
                     
-                    log_message(f'r/{subreddit} threads @ {timestamp}, {10_000 / seconds_elapsed:.2f} tps.')
+#                     log_message(f'r/{subreddit} threads @ {timestamp}, {10_000 / seconds_elapsed:.2f} tps.')
 
-            break
+#             break
 
-        except Exception as e:
-            log_message(e)
-            continue
+#         except Exception as e:
+#             log_message(e)
+#             continue
 
-    log_report(f'r/{subreddit} threads completed.')
+#     log_report(f'r/{subreddit} threads completed.')
 
 
 while True:
@@ -137,9 +141,8 @@ while True:
 
         collect_comments(subreddit)
 
-        log_message(f'Collecting r/{subreddit} threads.')
-
-        collect_threads(subreddit)
+        # log_message(f'Collecting r/{subreddit} threads.')
+        # collect_threads(subreddit)
 
     log_message('Sleeping for an hour.')
     log_report(f'Completed sweep.')
