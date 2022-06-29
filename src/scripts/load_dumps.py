8945298d524a98fd18c46073e873fa01f7a23489
Jackie Chan <jackiec1998@gmail.com>
2022-06-29 14:40:07 -0500

    Made it so it only queries subreddits that are necessary, saves progress. All comments denote what file they came from, whether they've been removed, deleted, and when they've been loaded into the database.

--- src/scripts/load_dumps.py
+++ src/scripts/load_dumps.py
@@ -4,14 +4,14 @@ import os
 from pymongo import MongoClient
 import json
 import sys
-import pandas as pd
 import datetime as dt
 import traceback
 import time
+import pickle
 
 client = MongoClient('localhost', 27017)
-pushshift_comments = client.resilient.pushshift_comments
-# pushshift_comments = client.resilient.test_comments
+collection = client.resilient.comments
+popular_threads = client.resilient.popular_threads
 
 def read_lines_zst(file_name):
     with open(file_name, 'rb') as file:
@@ -44,14 +44,13 @@ if __name__ == '__main__':
 
     os.chdir('/shared/jackie/resilient-communities/dumps/')
 
-    comments = []
-
-    file_name = 'RC_2022-03.zst'
+    file_name = 'RC_2021-06.zst'
     file_size = os.path.getsize(file_name)
     
     bytes_processed = 0
     file_lines = 0
     bad_lines = 0
+    skipped_lines = 0
 
     fields = [
         'author',
@@ -63,30 +62,58 @@ if __name__ == '__main__':
         'link_id',
         'parent_id',
         'permalink',
-        'retrieved_on',
+        'retrieved_utc',
         'score',
         'stickied',
         'subreddit'
     ]
 
-    # subreddits = ['UIUC']
+    if os.path.isfile('subreddits-loaded.pkl'):
+        with open('subreddits-loaded.pkl', 'rb') as file:
+            subreddits_loaded = pickle.load(file)
+    else:
+        subreddits_loaded = {}
+
+    subreddits = set(popular_threads.distinct('subreddit'))
+
+    if file_name in subreddits_loaded.keys():
+        subreddits = subreddits - subreddits_loaded[file_name]
+
+    subreddits = list(subreddits)
+
+    if not subreddits:
+        print('No subreddits to filter through.')
+        sys.exit()
 
     try:
         for line, bytes_processed in read_lines_zst(file_name):
             try:
+                loaded_one = False
+
                 comment = json.loads(line)
                 current = dt.datetime.fromtimestamp(int(comment['created_utc']))
 
+                if comment['subreddit'] not in subreddits:
+                    skipped_lines += 1
+                    continue
+
+                if 'retrieved_on' in comment.keys():
+                    comment['retrieved_utc'] = comment['retrieved_on']
+
                 comment = {key: comment[key] for key in fields}
+
+                comment['source'] = file_name
+                comment['removed'] = comment['body'] == '[removed]'
+                comment['deleted'] = comment['body'] == '[deleted]'
+                comment['loaded_utc'] = int(time.time())
+
                 while True:
 
                     attempts = 0
 
                     try:
-                        pushshift_comments.update_one({'id': comment['id']}, 
+                        collection.update_one({'id': comment['id']}, 
                             {'$set': comment}, upsert=True)
-
-                        # sys.exit()
                         break
                     except Exception as e:
                         attempts += 1
@@ -95,7 +122,6 @@ if __name__ == '__main__':
                             break
                         time.sleep(2)
                         continue
-
                 
             except (KeyError, json.JSONDecodeError) as e:
                 bad_lines += 1
@@ -105,11 +131,23 @@ if __name__ == '__main__':
             if file_lines % 10_000 == 0:
                 date_format = '%Y-%m-%d %H:%M:%S'
                 print(f'{current.strftime(date_format)} | '
-                     f'{file_lines:,} lines read | '
-                     f'{bad_lines:,} bad lines read | '
+                     f'{file_lines:,} read | '
+                     f'{bad_lines:,} bad | '
+                     f'{skipped_lines:,} skipped | '
                      f'{(bytes_processed / file_size) * 100:.2f}%')
 
     except Exception as e:
         print(e)
         traceback.print_exc()
         sys.exit()
+
+    if file_name in subreddits_loaded.keys():
+        subreddits_loaded[file_name] = \
+            subreddits_loaded[file_name].union(set(subreddits))
+    else:
+        subreddits_loaded[file_name] = set(subreddits)
+
+    with open('subreddits-loaded.pkl', 'wb') as file:
+        pickle.dump(subreddits_loaded, file)
+
+    print(f'Completed @ {dt.datetime.now()} | {file_lines:,} lines read.')    
